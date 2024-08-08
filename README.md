f48ffcf1fea371d9e6ecd5c610a59ea0f991ce58
Somik <somik123@users.noreply.github.com>
2024-08-08 08:49:29 +0800

    Security fixes & caching

--- README.md
+++ README.md
@@ -4,19 +4,48 @@ Python script with playwright, all packaged into a docker container with port 80
 ### Installation:
 Pull and deploy the image on your localhost's port 8080 with:
 ```
-docker run --name pyweb-capture -p 8080:8080 -d somik123/pyweb-capture:latest
+volumes:
+  pyweb_capture_data:
+    name: pyweb_capture_data
+
+services:
+  pyweb-capture:
+    restart: always
+    container_name: pyweb-capture
+    image: somik123/pyweb-capture:latest
+    volumes:
+      - pyweb_capture_data:/app/data
+    environment:
+      PYCAPTURE_SECRET: 1da12b5f29460177dcffbb3dd
+      PYCAPTURE_CACHE: yes
+      PYCAPTURE_DATA: /app/data
+    ports:
+      - 8080:8080
 ```
 
 ### Usage:
 Replace `10.0.0.10` with the IP address or host of your machine.
 
+Query parameters:
+```
+secret  - Required to provide if you set it in the env variable for the container.
+url     - The url to the website you want to screenshot
+full    - Set it if you want to capture the full page screeenshot
+refresh - Set it if you want to refresh this specific screenshot
+```
+
 Capture full screen screenshot of a website: 
 ```
-http://10.0.0.10:8080/?full&url=https://github.com/somik123/python-web-capture
+http://10.0.0.10:8080/?secret=1da12b5f29460177dcffbb3dd&full&url=https://github.com/somik123/python-web-capture
 ```
 
 
 Capture simple screenshot of a website: 
 ```
-http://10.0.0.10:8080/?url=https://github.com/somik123/python-web-capture
+http://10.0.0.10:8080/?secret=1da12b5f29460177dcffbb3dd&url=https://github.com/somik123/python-web-capture
+```
+
+Refresh screenshot of a website (instead of loading it from cache again):
+```
+http://10.0.0.10:8080/?secret=1da12b5f29460177dcffbb3dd&refresh&url=https://github.com/somik123/python-web-capture
 ```
--- script.py
+++ script.py
@@ -1,15 +1,20 @@
 from playwright.sync_api import sync_playwright
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, unquote, parse_qsl
-import json, argparse, subprocess, validators
+import json, argparse, subprocess, validators, os, hashlib
 
-def takeScreenshot(url, full):
+# get env vars
+secret = os.environ.get("PYCAPTURE_SECRET", "")
+save_dir = os.environ.get("PYCAPTURE_DATA", "/tmp")
+cache = os.environ.get("PYCAPTURE_CACHE", "no")
+
+# Use playwright to capture a screenshot of the page
+def takeScreenshot(url, save_file, full):
     with sync_playwright() as playwright:
-        #for browser_type in [p.chromium, p.firefox, p.webkit]:
         browser = playwright.chromium.launch()
         page = browser.new_page()
         page.goto(url)
-        page.screenshot(path='/tmp/screenshot.png', full_page=full)
+        page.screenshot(path=save_file, full_page=full)
         browser.close()
 
 
@@ -17,7 +22,9 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         url = ""
         full = False
+        refresh = False
         try:
+            # Parse the url into queries
             query = {}
             parsed_path = urlparse(self.path)
             index = self.path.index('?') + 1
@@ -27,23 +34,43 @@ class MyServer(BaseHTTPRequestHandler):
                 query[x[0]] = x[1]
             print(query)
 
+            # validate the url and query parameters
             tmp_url = unquote( query["url"] )
             if "full" in query.keys():
                 full = True
+            if "refresh" in query.keys():
+                refresh = True
             parsed_url = urlparse(tmp_url)
             if validators.url(tmp_url) and parsed_url.scheme == "https" :
-                url = tmp_url
+                if len(secret) > 0 and secret == query["secret"]:
+                    url = tmp_url
         except:
            print("Exception validator")
 
+        # Data OK
         self.send_response(200)
         if len(url) > 10:
-            self.send_header('Content-Type', 'image/png')
-            self.end_headers()
-            takeScreenshot(url, full)
-            with open("/tmp/screenshot.png", "rb") as image_file:
-                self.wfile.write(image_file.read())
+            # If image is cached, get from there instead
+            file_name = "screenshot.png"
+            if cache.lower() == "yes" :
+                file_name = hashlib.sha256(url.encode()).hexdigest() + ".png"
+            save_file = save_dir + "/" + file_name
+            try:
+                # If image is not cached, cache it (if enabled)
+                if refresh or not os.path.exists(save_file):
+                    takeScreenshot(url, save_file, full)
+
+                # Start returning image
+                with open(save_file, "rb") as image_file:
+                    self.send_header('Content-Type', 'image/png')
+                    self.end_headers()
+                    self.wfile.write(image_file.read())
+            except:
+                self.send_header('Content-Type', 'text/plain')
+                self.end_headers()
+                self.wfile.write("ERROR".encode('utf-8'))
         else:
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
             self.wfile.write("ERROR".encode('utf-8'))
         return
